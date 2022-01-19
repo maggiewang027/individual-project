5ad24f1a9f6f7a2b98e7a0053a764f232080dad9
Qifei Wang <94302872+maggiewang027@users.noreply.github.com>
2022-01-19 21:39:38 +0000

    Update User.hs

--- src/User.hs
+++ src/User.hs
@@ -5,20 +5,21 @@ module User (
 
 -- | Define a data type User, each user has a username of type String
 data User = User {
-    username :: String
+    username :: String,
+    userid :: Int
     } deriving (Show)
 
 -- | Declare 10 users
 let
-    user1 = User { username = "@tom123" }
-    user2 = User { username = "@maggie999" }
-    user3 = User { username = "@eliza315" }
-    user4 = User { username = "@jeremy01" }
-    user5 = User { username = "@john1996" }
-    user6 = User { username = "@qianyi0706" }
-    user7 = User { username = "@marykim6" }
-    user8 = User { username = "@miyeon09" }
-    user9 = User { username = "@rhea888" }
-    user10 = User { username = "@robertmay20" }
+    user1 = User { username = "@tom123", userid = 1 }
+    user2 = User { username = "@maggie999", userid = 2 }
+    user3 = User { username = "@eliza315", userid = 3 }
+    user4 = User { username = "@jeremy01", userid = 4 }
+    user5 = User { username = "@john1996", userid = 5 }
+    user6 = User { username = "@qianyi0706", userid = 6 }
+    user7 = User { username = "@marykim6", userid = 7 }
+    user8 = User { username = "@miyeon09", userid = 8 }
+    user9 = User { username = "@rhea888", userid = 9 }
+    user10 = User { username = "@robertmay20", userid = 10 }
 generateUsers :: [User]
 generateUsers = [user1, user2, user3, user4, user5, user6, user7, user8, user9, user10]
