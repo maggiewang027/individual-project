b23fc5d0f72f2751bf1b89b54cd9cfe8886efa28
Qifei Wang <94302872+maggiewang027@users.noreply.github.com>
2022-01-20 18:49:09 +0000

    Update Message.hs

--- src/Message.hs
+++ src/Message.hs
@@ -8,15 +8,14 @@ import User
 import Control.Concurrent
 import System.Random
 
--- | Define a type Message, each has a message of type String, a senderid of type Int
--- | and a receiverid of type Int
+-- | Define a type Message, each has a message of type String, a senderid of type Int and a receiverid of type Int
 data Message = Message {
     message :: String,
     senderId :: Int,
     receiverId :: Int
     } deriving (Show)
 
--- | Generate a list of messages from the .txt document
+-- | Transfer a list of messages from the .txt document
 messageList :: FilePath -> IO [String]
 messageList filename = do
     text <- readFile filename
@@ -35,12 +34,15 @@ receiver sendId = do
     randomId <- randomRIO (1, 10) :: IO Int
     if randomId /= sendId then return randomId else receiver sendId
 
--- | Start send message
-sendMessage :: MVar [Message] -> Int -> IO ()
-sendMessage box sendId = do
+-- | Start sending message
+sendMessage :: Int -> MVar [Message] -> IO ()
+sendMessage sendId box = do
     t <- randomRIO (10, 1000) :: IO Int
     threadDelay t
+    -- obtain a random receiver from function receiver
     receiveId <- receiver sendId
+    -- first load the .txt document
+    -- obtain a random message from function selectMessage
     randomText <- selectMessage =<< messageList "randomMessage.txt"
     let messages = Message { message = randomText, senderId = sendId, receiverId = receiveId }
     m <- takeMVar box
