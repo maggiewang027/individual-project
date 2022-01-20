980fb07701e013e5d2146805349cd8a4c90e1da9
Qifei Wang <94302872+maggiewang027@users.noreply.github.com>
2022-01-20 00:35:09 +0000

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,41 @@
-module Main where
+module Message (
+    Message (..),
+    receiver,
+    sendMessage,
+    countMessage
+    ) where
 
 import User
-import Message
-import Control.Monand
 import Control.Concurrent
 import System.Random
 
-main :: IO ()
-main = do
-    box <- newMVar []
-    putStrLn "Start sending messages..."
-    mapM_ (\ids -> forkIO (replicateM_ 10 (sendMessage box ids))) [1..10]
-    messages <- readMVar box
-    putStrLn "Start printing total number of messages of each user..."
-    mapM_ (countMessage ms) userList
-    putStrLn "Finished."
+-- | Define a type Message, each has a message of type String, a senderid of type Int
+-- | and a receiverid of type Int
+data Message = Message {
+    message :: String,
+    senderId :: Int,
+    receiverId :: Int
+    } deriving (Show)
+
+-- | Select a random receiver we send the message to
+receiver :: Int -> IO Int
+receiver sendId = do
+    randomId <- randomRIO (1, 10) :: IO Int
+    if randomId == sendId then receiver sendId else return randomId
+    
+-- | Start send message
+sendMessage :: MVar [Message] -> Int -> IO ()
+sendMessage box sendId = do
+    t <- randomRIO (10, 1000) :: IO Int
+    threadDelay t
+    receiveId <- receiver sendId
+    let messages = Message { message = "hi", senderId = sendId, receiverId = receiveId }
+    m <- takeMVar box
+    putMVar box ([messages] ++ m)
+
+    
+-- | Count the total number of messages each user received
+countMessage :: [Message] -> User -> IO ()
+countMessage ms user = do
+    let totalMessage = length $ filter (\someone -> receiverId someone == userId user ) ms
+    putStrLn $ "User " ++ (userName user) ++ " with user id " ++ (show $ userId user) ++ " receives " ++ (show totalMessage) ++ " messages in total."
