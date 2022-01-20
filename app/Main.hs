e07c775326f0fd76c368bccf789b86dc99b8eeb5
Qifei Wang <94302872+maggiewang027@users.noreply.github.com>
2022-01-20 01:02:46 +0000

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -6,11 +6,14 @@ import Control.Monad
 import Control.Concurrent
 import System.Random
 
+threadNumber = 10
+initialMessage = []
+
 main :: IO ()
 main = do
-    box <- newMVar []
+    box <- newMVar initialMessage
     putStrLn "Start sending messages..."
-    mapM_ (\ids -> forkIO (replicateM_ 10 (sendMessage box ids))) [1..10]
+    mapM_ (\ids -> forkIO (replicateM_ threadNumber (sendMessage box ids))) [1..10]
     threadDelay (110000)
     ms <- readMVar box
     putStrLn "Start printing total number of messages of each user..."
